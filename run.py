f6fd67e4029e4bb3245cd6fc5d5609d84a4d672b
gregp1985 <greg@gwphillips.co.uk>
2025-10-19 14:18:55 +0100

    data validation loop

--- run.py
+++ run.py
@@ -18,14 +18,17 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market")
-    print("Data should be six numbers, seperated by commas.")
-    print("example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market")
+        print("Data should be six numbers, seperated by commas.")
+        print("example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here:")
+        data_str = input("Enter your data here:")
     
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            print("Data is Valid")
+            break
 
 def validate_data(values):
     """
@@ -34,9 +37,12 @@ def validate_data(values):
     """
 
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(f"Exactly 6 valuies required, you provided {len(values)}")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True
 
-get_sales_data()
+data = get_sales_data()
